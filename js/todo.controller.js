92e3732b48bfb4df0118146a5e3c0178b5511554
Karoline <karolinedalencar@gmail.com>
2018-05-02 12:04:11 -0300

    Finish Remaining Functionality

--- js/todo.controller.js
+++ js/todo.controller.js
@@ -23,7 +23,13 @@ function TodoController() {
 
     this.removeTodo = function(item, index) {
         this.list.splice(index, 1);
-    }
+    };
+
+    this.getRemaining = function() {
+        return this.list.filter(function(item) {
+            return !item.completed;
+        });
+    };
     
 }
 
